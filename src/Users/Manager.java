8b7521d087eced3113c9cbde35f5f32608f368ae
Juhi Madhwani <jhmadhwa@ncsu.edu>
2019-04-06 23:28:04 -0400

    Added Manager views Staff info

--- src/Users/Manager.java
+++ src/Users/Manager.java
@@ -37,6 +37,10 @@ public class Manager {
 			case 1: 
 				viewPatients(conn,person_id);
 				break;
+			
+			case 2:
+				viewStaff(conn,person_id);
+				break;
 				
 			default:
 				System.out.println("Enter Valid choice");
@@ -67,6 +71,46 @@ public class Manager {
 				System.out.println("GENDER : " + rs.getString("GENDER"));
 				System.out.println("PROCESSING TREATMENT PLAN : " + rs.getInt("PROCESSING_TREATMENT_PLAN"));
 				System.out.println("STATUS : " + rs.getString("STATUS"));
+				System.out.println("\n");
+				
+			}
+			
+			System.out.println("Press 0 to go back");
+			int choice = sc.nextInt();
+			if (choice == 0) {
+				managerMenu(conn, person_id);
+			}
+			else
+			{
+				validChoice(0);
+				managerMenu(conn, person_id);
+			}
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public static void viewStaff(Connection conn, String person_id) {
+		try {
+			System.out.println("Enter Role :-> ");
+			String role = sc.next();
+			PreparedStatement stmt = conn.prepareStatement("SELECT SID,NAME,AGE,GENDER,JOB_TITLE,PROFESSIONAL_TITLE,PHONE_NUMBER,ADDRESS,DEPARTMENT FROM STAFF WHERE JOB_TITLE=?");
+			
+			stmt.setString(1, role);
+			
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				System.out.println("SID : " + rs.getInt("SID"));
+				System.out.println("NAME : " + rs.getString("NAME"));
+				System.out.println("AGE : " + rs.getInt("AGE"));
+				System.out.println("GENDER : " + rs.getString("GENDER"));
+				System.out.println("JOB_TITLE : " + rs.getString("JOB_TITLE"));
+				System.out.println("PROFESSIONAL TITLE : " + rs.getString("PROFESSIONAL_TITLE"));
+				System.out.println("PHONE NUMBER : " + rs.getString("PHONE_NUMBER"));
+				System.out.println("ADDRESS : " + rs.getString("ADDRESS"));
+				System.out.println("DEPARTMENT : " + rs.getString("DEPARTMENT"));
+				System.out.println("\n");
 				
 			}
 			
